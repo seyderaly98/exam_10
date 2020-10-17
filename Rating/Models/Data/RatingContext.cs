97712e31a6924be03dbfb353663b2e1e983f7dd5
Almat <seyderaly98@gmail.com>
2020-10-17 12:19:47 +0600

    #1 Добавил RatingContext унаследованный от IdentityDbContext<IdentityUser>

---
+++ Rating/Models/Data/RatingContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Rating.Models.Data
+{
+    public class RatingContext : IdentityDbContext<IdentityUser>
+    {
+        
+        public RatingContext(DbContextOptions<RatingContext> options) : base(options) {}
+    }
+}
