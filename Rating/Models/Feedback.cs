9f9f0dea4da8bf391862f38264e12c2604defb78
Almat <seyderaly98@gmail.com>
2020-10-17 13:49:24 +0600

    #5 Добавил сущность заведения и сущность отзовик к этому заведению

---
+++ Rating/Models/Feedback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Rating.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public int Estimation { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int InstitutionsId { get; set; }
+        public string UserId { get; set; }
+        
+        
+        [NotMapped]
+        public Institutions Institutions { get; set; }
+        [NotMapped]
+        public IdentityUser User { get; set; }
+        
+        
+        
+    }
+}
