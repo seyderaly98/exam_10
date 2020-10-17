0c151a11fd48e6ef74dea2f85ab431e5cb476aee
Almat <seyderaly98@gmail.com>
2020-10-17 13:00:41 +0600

    #4 Функционал разделение по ролям реализовано, при старте зарегистрируется супер админ

--- Rating/Program.cs
+++ Rating/Program.cs
@@ -3,17 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Rating.Services;
 
 namespace Rating
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var userManage = services.GetRequiredService<UserManager<IdentityUser>>();
+                var roleManage = services.GetRequiredService<RoleManager<IdentityRole>>();
+                await RoleInitializer.Initialize(userManage,roleManage);
+            }
+            catch (Exception e)
+            {
+                // var logger = services.GetRequiredService<ILogger<Program>();
+                // logger.LogError(e, "Возникло исключеие при инициализации ролей");
+                Console.WriteLine(e.Message);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Rating/Services/RoleInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Rating.Services
+{
+    public class RoleInitializer
+    {
+        public static async Task Initialize(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            
+            var roles = new[] { "Admin","User"};
+            foreach (var value in roles)
+            {
+                string role = Convert.ToString(value);
+                if (await roleManager.FindByNameAsync(role) is null)
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+            string adminEmail = "admin@admin.com";
+            string adminPassword = "Admin123";
+            if (await userManager.FindByEmailAsync(adminEmail) is null)
+            {
+                IdentityUser admin = new IdentityUser
+                {
+                    Email = adminEmail,
+                    EmailConfirmed = true,
+                    UserName = "Admin",
+                };
+                var result = await userManager.CreateAsync(admin, adminPassword);
+                if (result.Succeeded)
+                    await userManager.AddToRoleAsync(admin,"Admin");
+            }
+
+        }
+    }
+}
