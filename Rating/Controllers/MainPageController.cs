b967d0dc05ec14d05fb45f5a959414fe3170b7ad
Almat <seyderaly98@gmail.com>
2020-10-17 15:55:53 +0600

    Подключен сервер к view pageMain

--- Rating/Controllers/MainPageController.cs
+++ Rating/Controllers/MainPageController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rating.Models;
+using Rating.Models.Data;
 
 namespace Rating.Controllers
 {
     public class MainPageController : Controller
     {
-        // GET
-        public IActionResult Index()
+        public UserManager<IdentityUser> _userManager { get; set; }
+        public RoleManager<IdentityRole> _roleManager { get; set; }
+        public RatingContext _db { get; set; }
+
+        public MainPageController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, RatingContext db)
         {
-            return View();
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            List<Institution> institutions = await _db.Institutions.ToListAsync();
+            return View(institutions);
         }
     }
 }
