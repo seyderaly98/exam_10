bf7632ddc9fae9b8a3877857e74adb50d90a6c06
Almat <seyderaly98@gmail.com>
2020-10-17 12:54:27 +0600

    #3 Добавлена валидация регистрации пользователя

---
+++ Rating/Controllers/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Rating.Models.Data;
+
+namespace Rating.Controllers
+{
+    public class Validation : Controller
+    { private RatingContext _db;
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _signInManager;
+
+        public Validation(RatingContext db, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // GET
+        public async Task<bool> CheckEmail(string email)
+        {
+            return !await _db.Users.AnyAsync(u => u.Email == email);
+        }
+       
+    }
+}
