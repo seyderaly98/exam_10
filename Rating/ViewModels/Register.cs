3506f286c763f9e9381a84a1c46deae2a279aa8b
Almat <seyderaly98@gmail.com>
2020-10-17 12:51:08 +0600

    #2 Добавил class Register.cs и view Register.cshtml

---
+++ Rating/ViewModels/Register.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rating.ViewModels
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Это поле необходимо заполнить.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Это поле необходимо заполнить.")]
+        [EmailAddress(ErrorMessage = "Пожалуйста, введите действительный адрес электронной почты.")]
+        [Remote("CheckEmail","Validation",ErrorMessage = "Данный электронный адрес используется другим аккаунтом.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Это поле необходимо заполнить.")]
+        [DataType(DataType.Password)]
+        [MinLength(8,ErrorMessage = "Пароль должен содержать не менее 8 символов.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Это поле необходимо заполнить.")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "Пароли не совпадают.")]
+        [MinLength(8,ErrorMessage = "Пароль должен содержать не менее 8 символов.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
